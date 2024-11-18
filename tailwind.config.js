163bbe74f239295cb7ef8f9906a431dda8c48513
Aditya <adityanashrullah@gmail.com>
2024-11-18 13:00:12 +0700

    update dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    container: {
-      center: true,
-      padding: "16px",
+    content: ['./index.html'],
+    darkMode: 'class',
+    theme: {
+        container: {
+            center: true,
+            padding: '16px',
+        },
+        extend: {
+            colors: {
+                primary: '#0ea5e9',
+                secondary: '#64748b',
+                dark: '#0f172a',
+            },
+            screens: {
+                '2xl': '1320px',
+            },
+        },
     },
-    extend: {
-      colors: {
-        primary: "#0ea5e9",
-        secondary: "#64748b",
-        dark: "#0f172a",
-      },
-      screens: {
-        "2xl": "1320px",
-      },
-    },
-  },
-  plugins: [],
-};
+    plugins: [],
+}
